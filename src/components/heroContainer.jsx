02bd90d7a48e21d2eb9029ecd26f62f092c71b5f
lvivas <vivasleonardo4@gmail.com>
2024-01-17 21:26:39 -0300

    Hero section

---
+++ src/components/heroContainer.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+
+export default function heroContainer() {
+  return (
+    <>
+      <div className="div-2">
+        <img
+          loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F4f402b4c4b1b4db498df9c0ce1aef4d2"
+          className="img"
+          alt=" "
+        />
+        <div className="div-3">
+          <div className="div-4">
+            <img
+              loading="lazy"
+              srcSet="https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2Fb73c659ee6e9498595d83dff7270f9e8"
+              className="img-2"
+              alt=" "
+            />
+            <div className="div-5">
+              <div className="div-6">Inicio</div>
+              <div className="div-7">Nosotros</div>
+              <div className="div-8">Contacto</div>
+            </div>
+          </div>
+          <img
+            loading="lazy"
+            srcSet="https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Fed8b2ea899284c0685a527cf3cea6f83%2F1952e37fa9434b9e832a4fe5c488c9eb"
+            className="img-3"
+            alt=" "
+          />
+          <div className="div-10" />
+        </div>
+      </div>
+    </>
+  );
+}
